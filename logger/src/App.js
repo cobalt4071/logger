4842d2c1a0e0599a1981d188840f82e1610acdce
cobalt4071 <my.lib.1025@gmail.com>
2025-08-17 00:19:01 +0000

    issues

--- logger/src/App.js
+++ logger/src/App.js
@@ -27,17 +27,17 @@ import {
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-import FitnessCenterIcon from 'https://cdn.skypack.dev/@mui/icons-material/FitnessCenter?dts';
-import SettingsIcon from 'https://cdn.skypack.dev/@mui/icons-material/Settings?dts';
-import AddIcon from 'https://cdn.skypack.dev/@mui/icons-material/Add?dts';
-import TimerIcon from 'https://cdn.skypack.dev/@mui/icons-material/Timer?dts';
-import PlayArrowIcon from 'https://cdn.skypack.dev/@mui/icons-material/PlayArrow?dts';
-import StopIcon from 'https://cdn.skypack.dev/@mui/icons-material/Stop?dts';
-import PauseIcon from 'https://cdn.skypack.dev/@mui/icons-material/Pause?dts';
-import WatchLaterIcon from 'https://cdn.skypack.dev/@mui/icons-material/WatchLater?dts';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddIcon from '@mui/icons-material/Add';
+import TimerIcon from '@mui/icons-material/Timer';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import StopIcon from '@mui/icons-material/Stop';
+import PauseIcon from '@mui/icons-material/Pause';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
 
 // Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from "firebase/app";
 import {
   getFirestore,
   collection,
@@ -48,17 +48,17 @@ import {
   deleteDoc,
   doc,
   getDoc,
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+} from "firebase/firestore";
 import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
   onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+} from "firebase/auth";
 
 // Import the new WorkoutTracker component
-import WorkoutTracker from './WorkoutTracker.js';
+import WorkoutTracker from './WorkoutTracker';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -596,7 +596,6 @@ const App = () => {
     }
   
     showSnackbar(`Starting workout: ${workout.name}`, 'info');
-    // The previous code had a redundant 'return' here which was breaking the flow
   };
 
   // --- Pause/Resume Handler ---
